a514894707ee951b86ce74d42db4251624b0acff
Ronald Ordoñez <37679541+ronordonezq@users.noreply.github.com>
2020-01-08 10:21:08 -0500

    Archivos
    archivos iniciales

---
+++ app/src/main/java/com/example/tema1upt/MiSpinner.java
@@ -0,0 +1,14 @@
+package com.example.tema1upt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MiSpinner extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mi_spinner);
+    }
+}
